d1517138586a085fb40c362451b2534e67512483
Lucas Thompson <dev@lucas.im>
2016-08-19 10:48:59 +0100

    Abstract the audio data / wrap the Emscripten stuff in a JS class.

--- VampUtils.js
+++ VampUtils.js
@@ -1,28 +1,34 @@
 class VampPluginFeatureExtractor {
-  constructor(audioBuffer, vampJS, vampPlugin, pluginOutputNumber = 0) {
+  constructor(vampJS, vampPlugin, pluginOutputNumber = 0) {
     this.vampJS = vampJS;
-    this.audioBuffer = audioBuffer;
     this.pluginOutputNumber = pluginOutputNumber;
     this.vampPlugin = vampPlugin;
-    this.vampStream = this.vampJS.createRawDataAudioStream(this.audioBuffer.length, this.audioBuffer.numberOfChannels, this.audioBuffer.sampleRate);
   }
 
-  fillVampBuffer() {
-    for (let c = 0; c < this.audioBuffer.numberOfChannels; ++c) {
-      const channelBuffer = this.audioBuffer.getChannelData(c);
-      for (let n = 0; n < channelBuffer.length; ++n) {
-        vampStream.setSample(n, c, channelBuffer[n]);
-      }
-    }
-  }
-
-  extract() {
+  extract(audioStream) {
     const host = new this.vampJS.VampHost(this.vampPlugin);
-    this.fillVampBuffer();
-    const feature = JSON.parse(host.run(this.vampStream, this.vampJS.createJsonFeatureSetFormatter(this.pluginOutputNumber)));
+    audioStream.buffer();
+    const feature = JSON.parse(host.run(audioStream.vampStream, this.vampJS.createJsonFeatureSetFormatter(this.pluginOutputNumber)));
     const outputDescriptors = this.vampPlugin.getOutputDescriptors();
     const output = {descriptor: outputDescriptors.get(this.pluginOutputNumber), data: feature.feature[0].data[0]};
     host.delete();
     return output;
+  }
+}
+
+class AudioStream {
+  constructor(audioBuffer, vampJS) {
+    this.vampStream = vampJS.createRawDataAudioStream(audioBuffer.length, audioBuffer.numberOfChannels, audioBuffer.sampleRate);
+    this.numberOfChannels = audioBuffer.numberOfChannels;
+    this.audioBuffer = audioBuffer;
+  }
+
+  buffer() {
+    for (let c = 0; c < this.numberOfChannels; ++c) {
+      const channelBuffer = this.audioBuffer.getChannelData(c);
+      for (let n = 0; n < channelBuffer.length; ++n) {
+        this.vampStream.setSample(n, c, channelBuffer[n]);
+      }
+    }
   }
 }
